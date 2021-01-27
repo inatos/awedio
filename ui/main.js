f6e4e1b89d2f37ef7c1a680e1d92509cf2592827
inatos <32039018+inatos@users.noreply.github.com>
2021-01-26 21:36:30 -0500

    Test commit

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,13 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 
-/// Module to control application life.
+// Module to control application life.
 const { app } = electron;
 // Module to create native browser window.
 const { BrowserWindow } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// be closed automatically when the JavaScript object is garbage collected. 
 let mainWindow;
 
 function createWindow() {
